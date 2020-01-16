cacb01aa7e835acf25da0f867343d33c9987be67
Antonio Cruz and Benjamin Thom <benjamin@>
2020-01-16 13:23:59 -0800

    update styles.css and scripts.js with code for Subtract Form

--- js/scripts.js
+++ js/scripts.js
@@ -17,12 +17,20 @@ var divide = function (number1, number2) {
 };
 
 // This is all user interface, or front-end logic:
+
 $(document).ready(function() {
   $("form#add").submit(function(event) {
     event.preventDefault();
     var number1 = parseInt($("#add1").val());
     var number2 = parseInt($("#add2").val());
     var result = add(number1, number2);
+    $("#output").text(result);
+  });
+  $("form#subtract").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#subtract1").val());
+    var number2 = parseInt($("#subtract2").val());
+    var result = subtract(number1, number2);
     $("#output").text(result);
   });
 });
